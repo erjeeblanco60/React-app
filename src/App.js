2e68f8d3100de29fbbcdc37f5a80e66c9ac1613e
Erjee Conchas <Erjee>
2020-11-10 17:07:46 +0800

    Render data to component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-
+import React, {useState} from 'react'
 import './App.css';
 import GoalList from './components/GoalList';
 import NewGoals from './components/NewGoal/NewGoals'
 
+
 function App() {
-    const courseGoals = [
+   const [courseGoals,setCourseGoals] = useState([
       {id: 'CG1', text: 'finish course'},
       {id: 'CG2', text: 'Learn All about the course'},
       {id: 'CG3', text: 'Help other student in the Course'},
-    ]
+    ])
 
       const addNewGoalHandler = (newGoal) => { 
-        courseGoals.push(newGoal)
-        console.log(courseGoals)
+        setCourseGoals(courseGoals.concat(newGoal))
       }
 
   return (
       <> 
         <h2 className="course-goals"> Course Goals </h2>
+        {/* Accept the data from newGoals.js component and pass to addNewGoalHandler */}
         <NewGoals onAddGoal={addNewGoalHandler} />
         <GoalList goals={courseGoals} />
         
--- src/components/NewGoal/NewGoals.js
+++ src/components/NewGoal/NewGoals.js
@@ -3,7 +3,7 @@ import './css/NewGoal.css'
 
 const NewGoals = (props) => {
 
-
+    // submit FUNCTION Handler for form
     const addGoalHandler = (e) => {
         e.preventDefault();
        
@@ -11,12 +11,14 @@ const NewGoals = (props) => {
             id: Math.random().toString(),
             text: 'My New Goal'
         }
-        
+
+        //Pass the data to props.onAddgoals
         props.onAddGoal(newGoal);
     }
 
     return (
-        <> 
+        <>       
+                                                 {/* submit Handler for form                    */}
             <form className="new-goal-form" onSubmit={addGoalHandler}> 
                 <input type="text" /> 
                 <button type="submit">Add goals </button>
